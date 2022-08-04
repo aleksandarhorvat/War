d9ec050377c25393143f9bb4f1b249d4d9a8b57c
Aleksandar Horvat <aca.horvat.42@gmail.com>
2022-08-04 23:58:20 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace War
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbMyCard = new System.Windows.Forms.PictureBox();
+            this.btDraw = new System.Windows.Forms.Button();
+            this.pbHisCard = new System.Windows.Forms.PictureBox();
+            this.btAgain = new System.Windows.Forms.Button();
+            this.pbDeck1 = new System.Windows.Forms.PictureBox();
+            this.pbDeck2 = new System.Windows.Forms.PictureBox();
+            this.lbPoints2 = new System.Windows.Forms.Label();
+            this.lbPoints1 = new System.Windows.Forms.Label();
+            this.btShuffle = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMyCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHisCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDeck1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDeck2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbMyCard
+            // 
+            this.pbMyCard.Location = new System.Drawing.Point(216, 337);
+            this.pbMyCard.Margin = new System.Windows.Forms.Padding(4);
+            this.pbMyCard.Name = "pbMyCard";
+            this.pbMyCard.Size = new System.Drawing.Size(177, 250);
+            this.pbMyCard.TabIndex = 0;
+            this.pbMyCard.TabStop = false;
+            // 
+            // btDraw
+            // 
+            this.btDraw.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btDraw.Location = new System.Drawing.Point(216, 279);
+            this.btDraw.Margin = new System.Windows.Forms.Padding(4);
+            this.btDraw.Name = "btDraw";
+            this.btDraw.Size = new System.Drawing.Size(129, 28);
+            this.btDraw.TabIndex = 1;
+            this.btDraw.Text = "Draw the Card";
+            this.btDraw.UseVisualStyleBackColor = true;
+            this.btDraw.Click += new System.EventHandler(this.btDraw_Click);
+            // 
+            // pbHisCard
+            // 
+            this.pbHisCard.Location = new System.Drawing.Point(216, 44);
+            this.pbHisCard.Margin = new System.Windows.Forms.Padding(4);
+            this.pbHisCard.Name = "pbHisCard";
+            this.pbHisCard.Size = new System.Drawing.Size(177, 250);
+            this.pbHisCard.TabIndex = 2;
+            this.pbHisCard.TabStop = false;
+            // 
+            // btAgain
+            // 
+            this.btAgain.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btAgain.Location = new System.Drawing.Point(216, 279);
+            this.btAgain.Margin = new System.Windows.Forms.Padding(4);
+            this.btAgain.Name = "btAgain";
+            this.btAgain.Size = new System.Drawing.Size(129, 28);
+            this.btAgain.TabIndex = 3;
+            this.btAgain.Text = "Play Again";
+            this.btAgain.UseVisualStyleBackColor = true;
+            this.btAgain.Click += new System.EventHandler(this.btAgain_Click);
+            // 
+            // pbDeck1
+            // 
+            this.pbDeck1.Image = global::War.Properties.Resources.yellow_back;
+            this.pbDeck1.Location = new System.Drawing.Point(16, 337);
+            this.pbDeck1.Margin = new System.Windows.Forms.Padding(4);
+            this.pbDeck1.Name = "pbDeck1";
+            this.pbDeck1.Size = new System.Drawing.Size(177, 250);
+            this.pbDeck1.TabIndex = 5;
+            this.pbDeck1.TabStop = false;
+            // 
+            // pbDeck2
+            // 
+            this.pbDeck2.Image = global::War.Properties.Resources.red_back;
+            this.pbDeck2.Location = new System.Drawing.Point(16, 44);
+            this.pbDeck2.Margin = new System.Windows.Forms.Padding(4);
+            this.pbDeck2.Name = "pbDeck2";
+            this.pbDeck2.Size = new System.Drawing.Size(177, 250);
+            this.pbDeck2.TabIndex = 6;
+            this.pbDeck2.TabStop = false;
+            // 
+            // lbPoints2
+            // 
+            this.lbPoints2.AutoSize = true;
+            this.lbPoints2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPoints2.Location = new System.Drawing.Point(412, 101);
+            this.lbPoints2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbPoints2.Name = "lbPoints2";
+            this.lbPoints2.Size = new System.Drawing.Size(0, 135);
+            this.lbPoints2.TabIndex = 7;
+            // 
+            // lbPoints1
+            // 
+            this.lbPoints1.AutoSize = true;
+            this.lbPoints1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPoints1.Location = new System.Drawing.Point(412, 414);
+            this.lbPoints1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbPoints1.Name = "lbPoints1";
+            this.lbPoints1.Size = new System.Drawing.Size(0, 135);
+            this.lbPoints1.TabIndex = 8;
+            // 
+            // btShuffle
+            // 
+            this.btShuffle.AutoSize = true;
+            this.btShuffle.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btShuffle.Location = new System.Drawing.Point(16, 279);
+            this.btShuffle.Margin = new System.Windows.Forms.Padding(4);
+            this.btShuffle.Name = "btShuffle";
+            this.btShuffle.Size = new System.Drawing.Size(132, 28);
+            this.btShuffle.TabIndex = 9;
+            this.btShuffle.Text = "Shuffle the cards";
+            this.btShuffle.UseVisualStyleBackColor = true;
+            this.btShuffle.Click += new System.EventHandler(this.btShuffle_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResult.Location = new System.Drawing.Point(401, 302);
+            this.lbResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 25);
+            this.lbResult.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.versionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(627, 28);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.versionToolStripMenuItem.Text = "Version";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(118, 26);
+            this.toolStripMenuItem2.Text = "v1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(627, 594);
+            this.Controls.Add(this.btAgain);
+            this.Controls.Add(this.btDraw);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btShuffle);
+            this.Controls.Add(this.lbPoints1);
+            this.Controls.Add(this.lbPoints2);
+            this.Controls.Add(this.pbDeck2);
+            this.Controls.Add(this.pbDeck1);
+            this.Controls.Add(this.pbHisCard);
+            this.Controls.Add(this.pbMyCard);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(645, 641);
+            this.MinimumSize = new System.Drawing.Size(645, 641);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "War";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMyCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHisCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDeck1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDeck2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMyCard;
+        private System.Windows.Forms.Button btDraw;
+        private System.Windows.Forms.PictureBox pbHisCard;
+        private System.Windows.Forms.Button btAgain;
+        private System.Windows.Forms.PictureBox pbDeck1;
+        private System.Windows.Forms.PictureBox pbDeck2;
+        private System.Windows.Forms.Label lbPoints2;
+        private System.Windows.Forms.Label lbPoints1;
+        private System.Windows.Forms.Button btShuffle;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources; //added to use images from the resource
+
+namespace War
+{
+    public partial class Form1 : Form
+    {
+        private bool[] fillDeck = new bool[52]; //fill the deck with 52 cards
+
+        private Random rand = new Random(); //initialize a random value
+
+        private int myWins, hisWins;
+        private int myCard, hisCard; //initialize cards
+        private int myValue, hisValue; //initialize card values
+        private int myPoints, hisPoints; //initialize the points
+        private PictureBox[] selectCards; ////initialize card images
+
+        public Form1()
+        {
+            InitializeComponent();
+            myWins = hisWins = 0;
+            myCard = hisCard = -1; //cards get a value of -1 so they don't start at some value that is defined
+            myValue = hisValue = 0; //values are zero at the beginning
+            myPoints = hisPoints = 0; //points are zero at the beginning
+            btAgain.Visible = false; //we can't use the button to repeat if we haven't even started
+
+            selectCards = new PictureBox[52]; //we assign images to cards
+            ResourceManager rm = War.Properties.Resources.ResourceManager; //we take them from resources
+            for (int i = 0; i < 52; ++i) //we assign to each card
+            {
+                selectCards[i] = new PictureBox();
+                selectCards[i].Image = (Image)rm.GetObject("_" + (i + 1).ToString());
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("War is a game in which two players receive the same number of cards.\nBoth players draw cards and compare them.\nIf the first player's card has a higher value then it is put aside together with the second player's card, in case the cards are of equal value each player puts his side card.\nThe ace ranks as the strongest card, then all other cards in order from king to two.\nThe winner is the player with the most cards on the side when both players have used up all the cards in their decks.");
+        }
+
+        private void btDraw_Click(object sender, EventArgs e) //here we see what happens when we press the button to draw cards
+        {
+            btShuffle.Visible = false; //it is no longer available to us to shuffle the cards when we have already started to play
+            //get separate cards
+            do
+            {
+                //takes a value from 0 to 52
+                myCard = rand.Next(52);
+            }
+            while (fillDeck[myCard]); //until it gets a non-false bool value
+
+            pbMyCard.Image = new Bitmap(selectCards[myCard].Image); //we display a specific card image
+            fillDeck[myCard] = true; //states that the card has already been taken
+            myValue = (myCard % 13) + 2; //the value of the card is the remainder divided by 13 because each type of card has 13 cards, we add two more because we are working in 13 field and the value of the last card is 14
+            //we do the same for the computer
+            do
+            {
+                hisCard = rand.Next(52);
+            }
+            while (fillDeck[hisCard]);
+
+            pbHisCard.Image = new Bitmap(selectCards[hisCard].Image);
+            fillDeck[hisCard] = true;
+            hisValue = (hisCard % 13) + 2;
+
+            //divide the points
+            if (myValue > hisValue)
+                myPoints += 2;
+            else if (myValue < hisValue)
+                hisPoints += 2;
+            else
+            {
+                myPoints++;
+                hisPoints++;
+            }
+
+            //test whose victory
+            if ((myPoints + hisPoints) >= 52)
+            {
+                pbDeck1.Image = null;
+                pbDeck2.Image = null;
+                if (myPoints > hisPoints)
+                {
+                    lbResult.Text = "The first player wins!";
+                    myWins++;
+                }
+                else if (hisPoints > myPoints)
+                {
+                    lbResult.Text = "The other player won!";
+                    hisWins++;
+                }
+                else
+                    lbResult.Text = "A draw!";
+                btAgain.Visible = true;
+                btDraw.Visible = false;
+
+            }
+            //display points
+            lbPoints1.Text = Convert.ToString(myPoints);
+            lbPoints2.Text = Convert.ToString(hisPoints);
+        }
+
+        private void btAgain_Click(object sender, EventArgs e) //here everything is being adjusted again for the next match
+        {
+            myCard = hisCard = -1;
+            myValue = hisValue = 0;
+            myPoints = hisPoints = 0;
+            for (int i = 0; i < 52; ++i)
+                fillDeck[i] = false;
+            lbResult.Text = null;
+            lbPoints1.Text = null;
+            lbPoints2.Text = null;
+            pbDeck1.Image = War.Properties.Resources.yellow_back;
+            pbDeck2.Image = War.Properties.Resources.red_back;
+            pbMyCard.Image = pbHisCard.Image = null;
+            btDraw.Visible = true;
+            btAgain.Visible = false;
+            btShuffle.Visible = true;
+        }
+
+        private void btShuffle_Click(object sender, EventArgs e) //this is where the first card is shuffled
+        {
+            btShuffle.Visible = true;
+            do
+            {
+                myCard = rand.Next(52);
+            }
+            while (fillDeck[myCard]);
+
+            do
+            {
+                hisCard = rand.Next(52);
+            }
+            while (fillDeck[hisCard]);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) //the number of wins is displayed here after exiting the application
+        {
+            MessageBox.Show("Results!\n\nThe number of wins of the first player: " + myWins + "\n\nNumber of wins by the other player: " + hisWins + "\n");
+            Application.Exit();
+        }
+
+    }
+}
